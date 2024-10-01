0b368e3d23334be2d523cea4854a5e5d8b22fb77
Tej Chajed <tchajed@gmail.com>
2024-09-30 22:23:49 -0500

    Use only v62 of github api

--- mailbot.go
+++ mailbot.go
@@ -19,7 +19,7 @@ import (
 	"time"
 
 	"github.com/bradleyfalzon/ghinstallation/v2"
-	"github.com/google/go-github/v65/github"
+	"github.com/google/go-github/v62/github"
 	"golang.org/x/crypto/acme/autocert"
 )
 
