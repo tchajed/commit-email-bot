f9c48f6cd2d044b875c4af490dd4f331d34c0d0f
Tej Chajed <tchajed@gmail.com>
2024-12-29 09:49:11 -0600

    Fix TLS host whitelist

--- mailbot.go
+++ mailbot.go
@@ -163,7 +163,7 @@ func main() {
 	certManager := autocert.Manager{
 		Cache:      autocert.DirCache(tlsKeysDir),
 		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist(Cfg.PersistPath, fmt.Sprintf("www.%s", Cfg.Hostname)),
+		HostPolicy: autocert.HostWhitelist(Cfg.Hostname, fmt.Sprintf("www.%s", Cfg.Hostname)),
 	}
 	// This HTTP handler listens for ACME "http-01" challenges, and redirects
 	// other requests. It's useful for the latter in production in case someone
